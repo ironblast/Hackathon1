5f505979bd87798ee46a3f5f935d4f021acc1aa1
Yvan De paep <depaep@hotmail.fr>
2018-04-06 11:11:14 +0200

    ajout choix perso,player1 et 2

--- charSelection.js
+++ charSelection.js
@@ -1,13 +1,18 @@
 const targetElement = document.getElementById('charSelection')
+let heroes
+let playerIndex = 0
+let choosenHeroes = [undefined,undefined]
 
 const createHero = hero => `
 <div class=col-3>
-<img class=card-img-top alt="${hero.name}'s photo" src="${hero.images.sm}" />
+<img class=card-img-top onclick="myFunction(this, ${hero.id});"
+alt="${hero.name}" src="${hero.images.sm}" />
 <div class="card-body">
-    <p class="card-text">${hero.name}</p>
+    <p class="card-text"></p>
 </div>
 <h2></h2>
 </div>`
+
 
 
 function getRandom(){
@@ -17,12 +22,34 @@ function getRandom(){
     }
     return array
 }
+function myFunction(image, heroId) {
+    const id = `expandedImg${playerIndex}`
+    const expandImg = document.getElementById(id);
+    const imgText = document.getElementById("imgtext");
+    const hero = heroes.find(objet => objet.id === heroId)
+    choosenHeroes[playerIndex] = hero
+    expandImg.src = image.src;
+    imgText.innerHTML = image.alt;
+    expandImg.parentElement.style.display = "block";
+    
+}
 
+function validPlayer(button) {
+  if(choosenHeroes[playerIndex] === undefined) {
+    return
+  }
+
+  button.disabled = true
+  playerIndex += 1
+  if (playerIndex === 2) {
+    console.log(choosenHeroes)
+  }
+}
 fetch('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json')
 .then(response => response.json())
 .then(allHeroes => {
     const indices = getRandom()
-    const heroes = allHeroes.filter(
+    heroes = allHeroes.filter(
       (hero, i) => indices.includes(i)
     )
     console.log(heroes)
@@ -33,6 +60,7 @@ fetch('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json')
 
     targetElement.innerHTML = html  
 })
+
 
   // C'EST LA QUE SE PASSENT LES CHOSES INTERESSANTES !
   // Le paramètre hero contient l'objet reçu, décodé à partir du JSON.
