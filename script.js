082d78cfd0cbea5b2463600b802f2aae7b21e8d0
Romain Vaur <romainwcs@gmail.com>
2018-04-06 02:43:13 +0200

    Refactor et ajout defense et fury

--- script.js
+++ script.js
@@ -1,85 +1,123 @@
+function getRndPlayer() {
+  return Math.floor(2 * Math.random())
+}
 
 const players = [
   {
     defenseEnabled: false,
-    turnsBeforeBerserk: 3
+    turnsBeforeFury: 3
   },
   {
     defenseEnabled: false,
-    turnsBeforeBerserk: 3
+    turnsBeforeFury: 3
   }
 ]
 
-
-  function updateLifePoints(playerNum, val) {
-    const progressBarId = `avancement-${playerNum}`
-    const ava = document.getElementById(progressBarId);
-    if((ava.value+val)<=ava.max && (ava.value+val)>=0) {
-      ava.value += val;
-    }
+function updateLifePoints(playerNum, val) {
+  const progressBarId = `avancement-${playerNum}`
+  const ava = document.getElementById(progressBarId);
+  ava.value += val;
+  if(ava.value > ava.max) {
+    ava.value = ava.max;
   }
+  if(ava.value < 0) {
+    ava.value = 0;
+  }
+}
 
-const p0attack = document.getElementById('player-attack-0');
-const p1attack = document.getElementById('player-attack-1');
-
-p0attack.addEventListener('click', evt => {
+function player0done() {
+  players[1].defenseEnabled = false
   p0attack.disabled = true
   p0potion.disabled = true
+  p0fury.disabled = true
   p0defense.disabled = true
   p1attack.disabled = false
   p1potion.disabled = false
   p1defense.disabled = false
-  updateLifePoints(1, -10)
-})
+  players[1].turnsBeforeFury--
+  if(players[1].turnsBeforeFury === 0) {
+    p1fury.disabled = false
+    players[1].turnsBeforeFury = 3
+  }
+}
 
-p1attack.addEventListener('click', evt => {
+function player1done() {
+  players[0].defenseEnabled = false
   p0attack.disabled = false
   p0potion.disabled = false
   p0defense.disabled = false
   p1attack.disabled = true
   p1potion.disabled = true
   p1defense.disabled = true
-  updateLifePoints(0, -10)
+  p1fury.disabled = true
+  players[0].turnsBeforeFury--
+  if(players[0].turnsBeforeFury === 0) {
+    p0fury.disabled = false
+    players[0].turnsBeforeFury = 3
+  }
+}
+
+const p0attack = document.getElementById('player-attack-0');
+const p1attack = document.getElementById('player-attack-1');
+
+p0attack.addEventListener('click', evt => {
+  const damage = players[1].defenseEnabled ? -5 : -10
+  updateLifePoints(1, damage)
+  player0done()
+})
+
+p1attack.addEventListener('click', evt => {
+  const damage = players[0].defenseEnabled ? -5 : -10
+  updateLifePoints(0, damage)
+  player1done()
+})
+
+const p0fury = document.getElementById('player-fury-0');
+const p1fury = document.getElementById('player-fury-1');
+
+p0fury.addEventListener('click', evt => {
+  const damage = players[1].defenseEnabled ? -10 : -20
+  updateLifePoints(1, damage)
+  player0done()
+})
+
+p1fury.addEventListener('click', evt => {
+  const damage = players[0].defenseEnabled ? -10 : -20
+  updateLifePoints(0, damage)
+  player1done()
 })
 
 const p0potion = document.getElementById('player-potion-0');
 const p1potion = document.getElementById('player-potion-1');
 
 p0potion.addEventListener('click', evt => {
-  p0attack.disabled = true
-  p0potion.disabled = true
-  p0defense.disabled = true
-  p1attack.disabled = false
-  p1potion.disabled = false
-  p1defense.disabled = false
-  modif(+30)
+  updateLifePoints(0, +30)
+  player0done()
 })
 p1potion.addEventListener('click', evt => {
-  p0attack.disabled = false
-  p0potion.disabled = false
-  p0defense.disabled = false
-  p1attack.disabled = true
-  p1potion.disabled = true
-  p1defense.disabled = true
-  modification(+30)
+  updateLifePoints(1, +30)
+  player1done()
 })
 
 const p0defense = document.getElementById('player-defense-0');
 const p1defense = document.getElementById('player-defense-1');
 
 p0defense.addEventListener('click', evt => {
-  p0attack.disabled = true
-  p0potion.disabled = true
-  p0defense.disabled = true
-  p1attack.disabled = false
-  p1potion.disabled = false
-  p1defense.disabled = false
+  players[0].defenseEnabled = true
+  player0done()
 })
 p1defense.addEventListener('click', evt => {
-  p0attack.disabled = false
-  p0potion.disabled = false
-  p0defense.disabled = false
+  players[1].defenseEnabled = true
+  player1done()
+})
+
+if(getRndPlayer() === 0) {
   p1attack.disabled = true
   p1potion.disabled = true
   p1defense.disabled = true
-})
+}
+else {
+  p0attack.disabled = true
+  p0potion.disabled = true
+  p0defense.disabled = true
+}
